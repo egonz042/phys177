5664c1db2dfab97c3f11ef5ee6209309e4af6006
egonz042 <egonz042@ucr.edu>
2015-04-12 21:31:08 -0700

    Week 2

---
+++ Week2/ex1.py
@@ -0,0 +1,50 @@
+"""
+Week 2 Example 1
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def trapFunc(x,y):
+	dx = (x[1] - x[0])			#also h, trap spacing
+	k = 1					#sum index
+	TI = dx * 0.5 * (y[0] + y[-1])		#start I with end values
+	kmax = x.size-1
+	for k in range(kmax):
+		TI += dx * y[k]
+		k += 1
+	return TI
+
+def sampFunc(w,z):
+	dw = (w[1] - w[0])
+	j = 1.
+	SI = dw * (1./3.) * (z[0] + z[-1])
+	jmax = w.size-1
+	for j in range(jmax):
+		if (j % 2 == 0):
+			SI += (4./3.) * dw * z[j]
+			j+=1
+		else:
+			SI += (2./3.) * dw * z[j]
+			j+=1
+	return SI
+
+
+def sampFuncVarRange(w,z,u):
+	dw = (w[1] - w[0])
+	j = 1.
+	SI = dw * (1./3.) * (z[0] + z[-1])
+	jmax = u
+	for j in range(jmax):
+		if (j % 2 == 0):
+			SI += (4./3.) * dw * z[j]
+			j+=1
+		else:
+			SI += (2./3.) * dw * z[j]
+			j+=1
+	return SI
+
+
+
+
---
+++ Week2/ex2.py
@@ -0,0 +1,35 @@
+"""
+Week 2 Exercise 2
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import ex1 as Int
+
+t,x = np.loadtxt('velocities.txt', unpack=True)
+
+T = Int.trapFunc(t,x)
+S = Int.sampFunc(t,x)
+
+print 'The Trapezoidal Integral for distance traveled is {0:.7}'.format(T)
+
+print 'The Sampson Integral for distance traveled is {0:.7}'.format(S)
+
+#distance curve
+
+i = 1
+j = t.size
+d = np.zeros(j)
+
+for i in range(1,j):
+	dS = Int.sampFuncVarRange(t,x,i)
+	d[i] += dS
+
+plt.subplot(2,1,1)
+plt.plot(t,x,'o-',color='red')
+plt.xlabel('Time [s]')
+plt.ylabel('Velocity [m/s]')
+plt.subplot(2,1,2)
+plt.plot(t,d,'o-',color='blue')
+plt.xlabel('Time [s]')
+plt.ylabel('Distance [m]')
