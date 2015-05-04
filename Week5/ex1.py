7d5169c537794ed62ded05de525673ceb9ecfdf8
egonz042 <egonz042@ucr.edu>
2015-05-03 17:32:11 -0700

    Update ex1.py

--- Week5/ex1.py
+++ Week5/ex1.py
@@ -17,6 +17,7 @@ x = np.arange(0,1,0.01)
 f = np.zeros(len(x))
 f[:] = poly(x[:])
 plt.plot(f)
+plt.show()
 plt.savefig('Polynomial.png',format='png')
 
 #Guess .03, .17, .38, .62, .83, .96
@@ -33,16 +34,14 @@ for i in range(l):
 	r0 = g[i].copy()
 
 	while (j==0):
-
 		h = 0.005				#h step in derivative
 		ff = poly(r0)				#P(x)
 		fp = (poly(r0+h) - poly(r0-h))/(2*h)	#P'(x)
-
+		r0 = r0 - (ff/fp)		#Newton's Method
 		check = abs(ff/fp)			#Error approx.
 		if check < 1e-10:
 			root[i] = r0
 			j = 1
 		else:
-			r0 = r0 - (ff/fp)
-
-print root		
+			pass
+print 'The roots of the polynomial are:\n {0:11}\n {1:11}\n {2:11} \n {3:11}\n {4:11}\n {5:11}\nWith an error less than 1e-10.'.format(root[0],root[1],root[2],root[3],root[4],root[5])
